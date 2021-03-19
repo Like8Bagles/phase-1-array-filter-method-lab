647ddc001739a30d6ca276272f7093114a571c6e
Anthony <anthonykmbanks@gmail.com>
2021-03-19 05:22:11 -0400

    commit tree 464faaf0837ef18eee824d94a22ce4c7383c9ada

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (match) { 
+        return match.toLowerCase() === name.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter( function (match) {
+        return match.toLowerCase().indexOf(string.toLowerCase()) == 0
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter( function (arrayFind) {
+        return arrayFind.name == name
+    })
+}
